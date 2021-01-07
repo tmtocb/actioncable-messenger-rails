caf7fe3772735b78e1b64ab190b265f6c30ba661
tmtocb <kittz.esport@gmail.com>
2021-01-07 18:00:48 +0100

    set-up look, controllers, current_users, relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   validates_uniqueness_of :nickname
+
+  def self.generate
+    adjectives = ['Ancient', 'Creative', 'Dangerous', 'Effective', 'Flying', 'Gilded']
+    nouns = ['Highway', 'Intern', 'Jackhammer', 'Lion', 'Master']
+    number = rand.to_s[2..4]
+    nickname = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(nickname: nickname)
+  end
 end
